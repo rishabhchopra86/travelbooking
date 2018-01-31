1a1068a5c3e94165272b3330dfdb7e13e574347f
Rishabh <lanetteam.rishabh@gmail.com>
2018-01-31 18:29:35 +0530

    WEB UI DESIGN 009

--- pnr.aspx.cs
+++ pnr.aspx.cs
@@ -4,11 +4,60 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text;
 
 public partial class pnr : System.Web.UI.Page
 {
+    static byte[] bytes = ASCIIEncoding.ASCII.GetBytes("ZeroCool");
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        try
+        {
+            Response.Write("Original String: ");
+            string cryptedString = Encrypt("fgh");
+            Response.Write("\nEncrypt Result: "+ cryptedString);
+            Response.Write("Decrypt Result: "+ Decrypt(cryptedString));
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Response.Write("From: "+ex.Source+".\nDetail: "+  ex.Message);
+        }
+       
+    }
+    public static string Encrypt(string originalString)
+    {
+        if (String.IsNullOrEmpty(originalString))
+        {
+            throw new ArgumentNullException
+                   ("The string which needs to be encrypted can not be null.");
+        }
+        DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+        MemoryStream memoryStream = new MemoryStream();
+        CryptoStream cryptoStream = new CryptoStream(memoryStream,
+            cryptoProvider.CreateEncryptor(bytes, bytes), CryptoStreamMode.Write);
+        StreamWriter writer = new StreamWriter(cryptoStream);
+        writer.Write(originalString);
+        writer.Flush();
+        cryptoStream.FlushFinalBlock();
+        writer.Flush();
+        return Convert.ToBase64String(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+    }
+    public static string Decrypt(string cryptedString)
+    {
+        if (String.IsNullOrEmpty(cryptedString))
+        {
+            throw new ArgumentNullException
+               ("The string which needs to be decrypted can not be null.");
+        }
+        DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+        MemoryStream memoryStream = new MemoryStream
+                (Convert.FromBase64String(cryptedString));
+        CryptoStream cryptoStream = new CryptoStream(memoryStream,
+            cryptoProvider.CreateDecryptor(bytes, bytes), CryptoStreamMode.Read);
+        StreamReader reader = new StreamReader(cryptoStream);
+        return reader.ReadToEnd();
     }
 }
